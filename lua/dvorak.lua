40a16b92d4c115f487130b9e8aebfb18edba045d
Dave <dev@skullandquills.cm>
2023-06-28 11:59:14 +0800

    Conform mappings to vimrc
    Both maps.lua and dvorak.lua

--- lua/dvorak.lua
+++ lua/dvorak.lua
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------
---  dvorak.lua v. 20230510.1
+--  dvorak.lua v.20230626.1
 --  Dvorak-friendly neovim remappings
 --  Lives in ~/.config/nvim/lua
 ------------------------------------------------------------------------------
@@ -10,62 +10,79 @@ local all_modes = {}
 
 local nox_modes = {
 
-  {"t", "gk",},
-  {"n",  "l",},
-  {"d",  "h",},
-  {"h", "gj",},   -- j,k default to screen lines not logical lines
-  {"gh", "j",},   -- prefix a 'g' to change
-  {"gt", "k",},
+    {"t", "gk",},
+    {"n",  "l",},
+    {"d",  "h",},
+    {"h", "gj",},   -- default to screen lines not logical lines
+    {"gh", "j",},   -- prefix a 'g' to jump logical lines
+    {"gt", "k",},
 
-  -- ^n -> next search result, ^p -> prev
-  {"<c-n>", "nzz",},
-  {"<c-p>", "Nzz",},
+    -- n is remapped, so <c-n> becomes 'next search result' (N is not remapped)
+    -- recenter screen when jumping to prev/next seach result
+    {"<c-n>", "nzz",},
+    {"N",     "Nzz",},
 
-  -- k -> d (mnemonic: k = 'kill')
-  {"k", "d",},
+    -- k -> d (mnemonic: k = 'kill')
+    {"k", "d",},
 
-  -- <space> accesses original binding
-  {"<space>d", "d",},
-  {"<space>h", "h",},
-  {"<space>t", "t",},
-  {"<space>n", "n",},
-  {"<space>m", "m",},
+    -- <space> accesses original binding
+    {"<space>d", "d",},
+    {"<space>h", "h",},
+    {"<space>t", "t",},
+    {"<space>n", "n",},
+    {"<space>m", "m",},
 
-  -- window navigation
-  -- {"<leader>wd", "<c-w>h", desc = "move left one window",},
-  {"<leader>wm", "<c-w>j", desc = "move down one window",},
-  {"<leader>wt", "<c-w>k", desc = "move up one window",},
-  {"<leader>wn", "<c-w>l", desc = "move right one window",},
+    -- split navigation
+    {"<c-w>t", "<c-w>k", desc = "move to next split up",   },
+    {"<c-w>h", "<c-w>j", desc = "move to next split down", },
+    {"<c-w>d", "<c-w>h", desc = "move to next split left", },
+    {"<c-w>n", "<c-w>l", desc = "move to next split right",},
+}
 
-  -- new convenience binding ('-' complements '_')
-  {"-", "g_", {desc = "go to last char on line",},},
+local ci_modes = {
+
 }
 
 -- insert mode only
 local i_mode = {
-  -- indent/dedent uses meta key instead of ctrl
-  {"<m-t>", "<c-t>", {desc = "indent one shiftwidth",},},
-  {"<m-d>", "<c-d>", {desc = "dedent one shiftwidth",},},
+    -- indent/dedent uses meta key instead of ctrl
+    {"<m-t>", "<c-t>", {desc = "indent one shiftwidth",},},
+    {"<m-d>", "<c-d>", {desc = "dedent one shiftwidth",},},
 
-  -- <c-d>, <c-v> deletes next (instead of prev) char/word
-  {"<c-d>", "<c-o>dl", {desc = "delete next char",},},
-  {"<c-v>", "<c-o>dw", {desc = "delete next word",},},
+    -- <c-d>, <c-v> deletes next (instead of prev) char/word
+    {"<c-d>", "<c-o>dl", {desc = "delete next char",},},
+    {"<c-v>", "<c-o>dw", {desc = "delete next word",},},
+}
+
+local n_mode = {
+    -- move line up/down, reindenting
+    {"<m-t>",     ":move .-2<cr>==", {desc = "move line up"},  },
+    {"<leader>t", ":move .-2<cr>==", {desc = "move line up"},  },
+    {"<m-h>",     ":move .+1<cr>==", {desc = "move line down"},},
+    {"<leader>h", ":move .+1<cr>==", {desc = "move line down"},},
+}
+
+local v_mode = {
+    -- move region up/down, reindenting
+    {"<m-t>", ":move '<-2<cr>gv=gv", {desc = "move region up"},  },
+    {"<m-h>", ":move '>+1<cr>gv=gv", {desc = "move region down"},},
 }
 
 
 --- Loop assignment ----------------------------------------------------------
 local mode_maps = {
-  {{"", "!",},    all_modes},
-  {{"",},         nox_modes},
-  {{"i",},           i_mode},
-  -- {{"c", "i", "n", "o", "x",}, all_modes},
-  -- {{"n", "o", "x",},           nox_modes},
+    {{"",},         nox_modes},
+    {{"!",},         ci_modes},
+    {{"i",},           i_mode},
+    {{"n",},           n_mode},
+    {{"v",},           v_mode},
+    {{"", "!",},    all_modes},
 }
 
 for _, mode_map in pairs (mode_maps) do
-  local modes, maps = unpack (mode_map)
-  if maps then
-    for __, map in pairs (maps) do apply_keymap (modes, map) end
-  end
+    local modes, maps = unpack (mode_map)
+    if maps then
+        for _, map in pairs (maps) do apply_keymap (modes, map) end
+    end
 end
 
--- lua/maps.lua
+++ lua/maps.lua
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------
---  maps.lua v. 20230418.1
+--  maps.lua v.20230626.1
 --  General neovim remappings
 --  Lives in ~/.config/nvim/lua
 ------------------------------------------------------------------------------
@@ -16,44 +16,50 @@ local apply_keymap  = require 'common'.apply_keymap
 
 --- Keymaps ------------------------------------------------------------------
 --  Build tables of key assignments, then loop-apply them
-local all_modes = {
-  {"<m-e>", "<esc>", {desc = "synonym for escape",},},
-}
+local all_modes = { }
 
-
---  "motion" modes
+--  "motion" modes (normal, visual, select, operator pending)
 local nox_modes = {
-  -- eliminate timeout lag
-  {";;",       ";", },
-  {";<space>", ";", },
-  {"<space>;", ";", },
-  {",,",       ",", },
-  {",<space>", ",", },
-  {"<space>,", ",", },
 
-  -- eliminate movement when <space> used as command prefix or terminator
-  {"<space>", "<nop>",  },
+    -- eliminate movement when <space> used as command prefix or terminator
+    {"<space>", "<nop>",  },
 
-  -- recenter screen on ^u or ^d
-  {"<c-b>", "<c-b>zz",  },
-  {"<c-d>", "<c-d>zz",  },
-  {"<c-f>", "<c-f>zz",  },
-  {"<c-u>", "<c-u>zz",  },
+    -- eliminate timeout lag
+    {";;",       ";", },
+    {";<space>", ";", },
+    {"<space>;", ";", },
+    {",,",       ",", },
+    {",<space>", ",", },
+    {"<space>,", ",", },
 
-  -- toggle numbering; <leader>r (relative), <leader>R (absolute)
-  {"<leader>r",   -- relative numbering
-    function()
-      vim.wo.relativenumber = not vim.wo.relativenumber
-    end,
-  },
+    -- recenter screen on ^u or ^d
+    {"<c-b>", "<c-b>zz",  },
+    {"<c-d>", "<c-d>zz",  },
+    {"<c-f>", "<c-f>zz",  },
+    {"<c-u>", "<c-u>zz",  },
 
-  {"<leader>R",   -- absolute numbering
-    function()
-      vim.wo.number = not vim.wo.number
-    end,
-  },
+    -- new convenience binding ('-' complements '_')
+    {"-", "g_", {desc = "go to last char on line",},},
+
+    -- toggle numbering
+    {"<leader>r",         -- relative numbering
+        function()
+            vim.wo.relativenumber = not vim.wo.relativenumber
+        end,
+    },
+
+    {"<leader>R",         -- absolute numbering
+        function()
+            vim.wo.number = not vim.wo.number
+        end,
+    },
+
+    {"<leader><space>r",  -- absolute numbering (alt)
+        function()
+            vim.wo.number = not vim.wo.number
+        end,
+    },
 }
-
 
 local ox_modes = {
   -- 'r' is already a defined text object in nvim-surround
@@ -61,128 +67,129 @@ local ox_modes = {
   -- {"ar", "a[", {desc = "around rectangular bracket text object"},},
 }
 
-
 -- normal but not visual/select modes
 local n_mode = {
 
-  -- telescope commands
-  --{";ff", ts.find_files},
-  --{";fg", ts.live_grep },
-  --{";fb", ts.buffers   },
+    -- telescope commands
+    --{";ff", ts.find_files},
+    --{";fg", ts.live_grep },
+    --{";fb", ts.buffers   },
 
-  -- nvim-tree commands
-  {";tt", function() cmd {cmd = 'NvimTreeToggle'} end,},
-  -- {";tf", function() cmd {cmd = 'NvimTreeFocus'} end,}, -- toggle focus
+    -- nvim-tree commands
+    {";tt", function() cmd {cmd = 'NvimTreeToggle'} end,},
+    -- {";tf", function() cmd {cmd = 'NvimTreeFocus'} end,}, -- toggle focus
 
-  {"Q", "gqap", {desc = "reformat paragraph"},},
+    -- recenter screen when jumping to prev/next seach result
+    -- (n is remapped in dvorak.lua)
+    {"n", "nzz",},    -- remapped in dvorak.lua
+    {"N", "Nzz",},
 
-  --  ^l clears highlighting
-  {"<c-l>",
-    function()
-      cmd.nohlsearch()
-      cmd.redraw()
-    end,
-    {desc = "clear search highlighting and redraw screen"},
-  },
+    {"Q", "gqap", {desc = "reformat paragraph"},},
 
-  {"<c-s>", cmd.write, {desc = "save buffer"},       },
+    --  ^l clears highlighting
+    {"<c-l>",
+        function()
+            cmd.nohlsearch()
+            cmd.redraw()
+        end,
+        {desc = "clear search highlighting and redraw screen"},
+    },
 
-  -- same effect as built-in <c-6> [<c-^>]
-  {"<leader>b",
-    function() cmd.buffer('#') end,
-    {desc = "alt buffer"},
-  },
+    {"<c-s>", cmd.write, {desc = "save buffer"},       },
 
-  -- delete current buffer if not modified
-  {"<leader>d",
-    function()
-      if fn.getbufinfo("%").changed then
-        print ("Buffer modified since last save")
-      elseif fn.bufexists (fn.bufname ("#")) then
-        cmd.buffer("#")
-        cmd.bdelete('#')
-      else
-        cmd.bdelete("%")
-      end
-    end,
-    {desc = "close current buffer, preserving window layout"},
-  },
+    -- open/close splits
+    {'<c-w>-', cmd.split,  {desc = "new horizontal split"},},
+    {'<c-w>/', cmd.vsplit, {desc = "new vertical split"},  },
+    {'<c-w>x', cmd.close,  {desc = "close split"},         },
 
-  -- delete current buffer, discarding modifications
-  {"<leader>D",
-    function()
-      if fn.bufexists (fn.bufname ("#")) then
-        cmd.buffer("#")
-        cmd.bdelete {args = {"#",}, bang = true,}
-      else
-        cmd.bdelete {args = {"%",}, bang = true,}
-      end
-    end,
-    {desc = "close current buffer, preserving window layout"},
-  },
+    -- delete current buffer if not modified
+    {"<leader>d",
+        function()
+            if fn.getbufinfo("%").changed then
+                print ("Buffer modified since last save")
+            elseif fn.bufexists (fn.bufname ("#")) then
+                cmd.buffer("#")
+                cmd.bdelete('#')
+            else
+                cmd.bdelete("%")
+            end
+        end,
+        {desc = "close current buffer, preserving window layout"},
+    },
 
-  {"<leader>p", cmd.bprevious, {desc = "prev buffer"}, },
+    -- delete current buffer, discarding modifications
+    {"<leader>D",
+        function()
+            if fn.bufexists (fn.bufname ("#")) then
+                cmd.buffer("#")
+                cmd.bdelete {args = {"#",}, bang = true,}
+            else
+                cmd.bdelete {args = {"%",}, bang = true,}
+            end
+        end,
+        {desc = "close current buffer, preserving window layout"},
+    },
 
-  {"<leader>l", cmd.ls, {desc = "list buffers"}, },
+    {"<leader>l", cmd.ls, {desc = "list buffers"}, },
 
-  {"<leader>so",
-    function()
-      cmd.source("%")
-      print (fn.printf ("'%s' sourced", fn.expand ("%:t")))
-    end,
-    {desc = "source active file",},
-  },
+    {"<leader>so",
+        function()
+            cmd.source("%")
+            print (fn.printf ("'%s' sourced", fn.expand ("%:t")))
+        end,
+        {desc = "source active file",},
+    },
 
-  -- use <leader> with tmux prefixes to create new splits
-  {'<leader>=', cmd.split,  {desc = "new horizontal split"},},
-  {'<leader>"', cmd.vsplit, {desc = "new vertical split"},  },
+    -- paste from clipboard
+    {"<m-p>",            '"+p', {desc = "paste below from clipboard"},  },
+    {"<leader>p",        '"+p', {desc = "paste below from clipboard"},  },
+    {"<leader>P",        '"+P', {desc = "paste above from clipboard"},  },
+    {"<leader><space>p", '"+P', {desc = "paste above from clipboard"},  },
 
-  -- <meta> key assignments
-  {"<m-t>", ":move .-2<cr>==", {desc = "move line up"},  },
-  {"<m-h>", ":move .+1<cr>==", {desc = "move line down"},},
+    -- move line up/down
+    {"<m-k>", ":move .-2<cr>==", {desc = "move line up"},  },
+    {"<m-j>", ":move .+1<cr>==", {desc = "move line down"},},
 }
 
-
-local i_mode = {
-  -- {"<m-t>", "<esc>:move '.-2<cr>==gi",
-  --   {desc = "move selected line/block up"},
-  -- },
-  -- {"<m-h>", ":<esc>move '.+1<cr>==gi",
-  --   {desc = "move selected line/block down"},
-  -- },
-}
-
+local i_mode = { }
 
 local v_mode = {
-  {"<", "<gv",},                    -- stay selected after indent/dedent
-  {">", ">gv",},
-  {"<m-k>", ":move '<-2<cr>gv=gv", {desc = "move selected line/block up"},  },
-  {"<m-j>", ":move '>+1<cr>gv=gv", {desc = "move selected line/block down"},},
+    -- stay selected after region indent/dedent
+    {"<", "<gv",},
+    {">", ">gv",},
+
+    -- move region up/down
+    {"<m-k>", ":move '<-2<cr>gv=gv", {desc = "move region up"},  },
+    {"<m-j>", ":move '>+1<cr>gv=gv", {desc = "move region down"},},
+
+    -- yank to clipboard
+    {"<m-y>",     '"+y', {desc = "yank to clipboard"},  },
+    {"<leader>y", '"+y', {desc = "yank to clipboard"},  },
 }
 
-
 local x_mode = {
-  {"il", "g_o^",},
-  {"al", "V",},
+    -- operate inside, around line
+    {"il", "g_o^",},
+    {"al", "V",},
 }
 
 
 --- Loop assignment ----------------------------------------------------------
 local mode_maps = {
-  {{"", "!",},    all_modes},   -- "" = :map, "!" = :map!
-  {{"",},         nox_modes},
-  {{"o", "x",},    ox_modes},
-  {{"i",},           i_mode},
-  {{"n",},           n_mode},
-  {{"v",},           v_mode},
-  {{"x",},           x_mode},
+    {{"", "!",},    all_modes},   -- "" = :map, "!" = :map!
+    {{"",},         nox_modes},
+    {{"o", "x",},    ox_modes},
+    {{"i",},           i_mode},
+    {{"n",},           n_mode},
+    {{"v",},           v_mode},
+    {{"x",},           x_mode},
 }
 
 for _, mode_map in pairs (mode_maps) do
-  local modes, maps = unpack (mode_map)
-  if maps then
-    for __, map in pairs (maps) do apply_keymap (modes, map) end
-  end
+    local modes, maps = unpack (mode_map)
+    if maps then
+        for __, map in pairs (maps) do apply_keymap (modes, map) end
+    end
 end
 
 
